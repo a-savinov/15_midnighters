ce64a73b99127ae493ce2681ba74a27cea711074
Alexey Savinov <belsat.net@gmail.com>
2017-10-11 18:34:47 +0300

    who are owls ?

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Finds who sent the tasks for verification from **00:00** till **05:00** and prints 
+result to the console
 
-# Project Goals
+### How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+### How to use
+```bash
+
+$ python seek_dev_nighters.py
+
+```
+
+### Sample output
+```bash
+$ python seek_dev_nighters.py
+This devman users are owls:
+smth_creative
+lekcrg
+AlexBald
+dm_shorokhov
+id5616980
+rzabaluev
+irinanizova0
+mmatveev
+АлексейЦыкунов
+b0r1soff
+chadarr
+madfoolish
+АнтонКурдин
+roman_muzzy
+```
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,11 +1,7 @@
+from datetime import datetime
+
+import pytz
 import requests
-
-'''
-import pytz, datetime
-
-tz          = pytz.timezone('Europe/Moscow')
-client_time = datetime.datetime.now(tz)
-'''
 
 
 def load_attempts():
@@ -13,8 +9,9 @@ def load_attempts():
     api_url = 'http://devman.org/api/challenges/solution_attempts'
     number_of_pages = requests.get(api_url).json()['number_of_pages']
     for page in range(number_of_pages):
-        page_url = '{}/?page={}'.format(api_url, int(page + start_page))
-        users_data = requests.get(page_url).json()['records']
+        page_num = page + start_page
+        users_data = requests.get(api_url,
+                                  params={'page': page_num}).json()['records']
         for user in users_data:
             yield {
                 'username': user['username'],
@@ -23,11 +20,23 @@ def load_attempts():
             }
 
 
-def get_midnighters():
-    pass
+def check_midnighter(user_info, start_hour=0, stop_hour=5):
+    user_timezone = pytz.timezone(user_info['timezone'])
+    attempt_time_utc = datetime.utcfromtimestamp(user_info['timestamp'])
+    return bool(pytz.utc.localize(attempt_time_utc, is_dst=None).astimezone(
+        user_timezone).hour in range(
+        start_hour, stop_hour))
 
+
+def output_users_to_console(midnigh_users):
+    print('This devman users are owls:')
+    for midnigh_user in midnigh_users:
+        print(midnigh_user)
 
 if __name__ == '__main__':
-    gen= load_attempts()
-    for g in gen:
-        print(g)
+    generator = load_attempts()
+    midnigh_users = set()
+    midnigh_users = {user['username'] for user in generator if user[
+        'timestamp'] and check_midnighter(user)}
+    output_users_to_console(midnigh_users)
+
