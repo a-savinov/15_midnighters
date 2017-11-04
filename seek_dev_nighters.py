c6beb01d9df0fdd373cea28973c777aef5bf4f73
Alexey Savinov <belsat.net@gmail.com>
2017-11-04 13:27:08 +0300

    optimize work with api, save one request

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,20 +4,28 @@ import pytz
 import requests
 
 
+def get_attempt_page(page_num):
+    api_url = 'http://devman.org/api/challenges/solution_attempts'
+    attempts_page_data = requests.get(api_url, params={'page': page_num})
+    return attempts_page_data.json()
+
+
 def load_attempts():
     start_page = 1
-    api_url = 'http://devman.org/api/challenges/solution_attempts'
-    number_of_pages = requests.get(api_url).json()['number_of_pages']
-    for page in range(number_of_pages):
-        page_num = page + start_page
-        users_data = requests.get(api_url,
-                                  params={'page': page_num}).json()['records']
-        for user in users_data:
+    while True:
+        raw_json_data = get_attempt_page(start_page)
+        number_of_pages = raw_json_data['number_of_pages']
+        attempts_data = raw_json_data['records']
+        for attempt in attempts_data:
             yield {
-                'username': user['username'],
-                'timestamp': user['timestamp'],
-                'timezone': user['timezone'],
+                'username': attempt['username'],
+                'timestamp': attempt['timestamp'],
+                'timezone': attempt['timezone'],
             }
+        if start_page < number_of_pages:
+            start_page = start_page + 1
+        else:
+            break
 
 
 def check_midnighter(attempt_info, start_hour=0, stop_hour=5):
@@ -32,6 +40,7 @@ def output_users_to_console(midnigh_users_name):
     print('This devman users are owls:')
     for midnigh_user_name in midnigh_users_name:
         print(midnigh_user_name)
+
 
 if __name__ == '__main__':
     attempts = load_attempts()
